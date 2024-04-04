055d3032f8c4e3627493ed2733b6736263d30026
thewoodfish <jasonholt2002@gmail.com>
2024-04-04 20:44:27 +0100

    init: setup the library and binary crate(for in-dev testing)

---
+++ client/src/main.rs
@@ -0,0 +1,6 @@
+
+
+fn main() {
+    println!("Hello, world!");
+    println!("{}", swarm_nl::add(23, 23));
+}
---
+++ swarm_nl/src/lib.rs
@@ -0,0 +1,18 @@
+/// Copyright (c) 2024 Algorealm
+/// 
+/// This is the main entry point for the SwarmNL library crate.
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
