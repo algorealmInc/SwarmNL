d400c9c100b082905201d246f916ccfa003ed85b
thewoodfish <jasonholt2002@gmail.com>
2024-04-21 11:22:33 +0100

    add: generate RSA keypair from binary crypto file

--- swarm_nl/src/prelude.rs
+++ swarm_nl/src/prelude.rs
@@ -61,41 +61,6 @@ impl CustomFrom for KeyType {
     }
 }
 
-/// A wrapper for the cryptographic Keypair for node identification and message signing.
-/// This is only necessary because of the distinction between an RSA keypair and the others
-#[derive(Debug, Clone)]
-pub enum WrappedKeyPair {
-    Rsa(RsaKeypair),
-    Other(Keypair),
-}
-
-impl WrappedKeyPair {
-    /// Return the internal RSA keypair if it exists
-    pub fn into_inner_rsa(&self) -> Option<RsaKeypair> {
-        if let WrappedKeyPair::Rsa(keypair) = self {
-            Some(keypair.clone())
-        } else {
-            None
-        }
-    }
-
-    /// Return the internal keypair that is not of the RSA type
-    pub fn into_inner(&self) -> Option<Keypair> {
-        if let WrappedKeyPair::Other(keypair) = self {
-            Some(keypair.clone())
-        } else {
-            None
-        }
-    }
-}
-
-/// Supported runtimes and executor
-#[derive(Hash, Eq, PartialEq)]
-pub enum Runtime {
-    AsyncStd,
-    Tokio,
-}
-
 /// Supported transport protocols
 #[derive(Hash, Eq, PartialEq, Debug)]
 pub enum TransportOpts {
