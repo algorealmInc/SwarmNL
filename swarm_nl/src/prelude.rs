5b64e3458aa385be5dec2bafebdcf06111b16930
sacha <23283108+sacha-l@users.noreply.github.com>
2024-05-06 18:44:14 +0200

    chore: run fmt on all codebase

--- swarm_nl/src/prelude.rs
+++ swarm_nl/src/prelude.rs
@@ -4,7 +4,6 @@ use std::net::Ipv4Addr;
 ///  
 /// This file is part of the SwarmNL library.
 use thiserror::Error;
-use std::net::Ipv4Addr;
 
 /// Default IP address when no address is specified.
 pub static DEFAULT_IP_ADDRESS: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
