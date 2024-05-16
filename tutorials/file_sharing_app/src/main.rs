fdfb2680888620a861e3007deb73a0402b5b395b
thewoodfish <jasonholt2002@gmail.com>
2024-05-16 20:28:44 +0100

    fix: change tokio to async-std runtime for example

--- tutorials/file_sharing_app/src/main.rs
+++ tutorials/file_sharing_app/src/main.rs
@@ -1,3 +1,10 @@
+/// Copyright (c) Algorealm 2024
+
+/// This crate demonstrates how to use SwarmNl. Here, we build a simple file sharing application
+/// using two nodes. One nodes writes a record to the DHT and specifies itself as a provider for a
+/// file it has locally. The other node reads the DHT and then uses an RPC to fetch the file from
+/// the first peer.
+/// 
 use std::{
 	collections::HashMap,
 	fs::File,
@@ -11,20 +18,14 @@ use swarm_nl::{
 	setup::BootstrapConfig,
 	ConnectedPoint, ConnectionId, Keypair, ListenerId, Multiaddr, PeerId, Port,
 };
-/// Copyright (c) Algorealm 2024
-
-/// This crate demonstrates how to use SwarmNl. Here, we build a simple file sharing application
-/// using two nodes. One nodes writes a record to the DHT and specifies itself as a provider for a
-/// file it has locally. The other node reads the DHT and then uses an RPC to fetch the file from
-/// the first peer.
 
 /// The key we're writing to the DHT
-pub const KADEMLIA_KEY: &str = "bootstrap_config.ini"; // File name
-pub const KADEMLIA_VALUE: &str = "bootstrap_config.ini"; // Location on fs (it is in the same directory as our binary)
+const KADEMLIA_KEY: &str = "config_file"; // File name
+const KADEMLIA_VALUE: &str = "bootstrap_config.ini"; // Location on fs (it is in the same directory as our binary)
 
 /// Our test keypair for node 1. It is always deterministic, so that node 2 can always connect to it
 /// at boot time
-pub const PROTOBUF_KEYPAIR: [u8; 68] = [
+const PROTOBUF_KEYPAIR: [u8; 68] = [
 	8, 1, 18, 64, 34, 116, 25, 74, 122, 174, 130, 2, 98, 221, 17, 247, 176, 102, 205, 3, 27, 202,
 	193, 27, 6, 104, 216, 158, 235, 38, 141, 58, 64, 81, 157, 155, 36, 193, 50, 147, 85, 72, 64,
 	174, 65, 132, 232, 78, 231, 224, 88, 38, 55, 78, 178, 65, 42, 97, 39, 152, 42, 164, 148, 159,
@@ -32,11 +33,12 @@ pub const PROTOBUF_KEYPAIR: [u8; 68] = [
 ];
 
 /// Node 1 wait time (for node 2 to initiate connection).
-/// This is useful because we need at least one connected peer (Quorum) to successfully write to the DHT
-pub const NODE_1_WAIT_TIME: u64 = 3;
+/// This is useful because we need at least one connected peer (Quorum) to successfully write to the
+/// DHT
+const NODE_1_WAIT_TIME: u64 = 5;
 
 /// Node 2 wait time (for node 1 to write to the DHT).
-pub const NODE_2_WAIT_TIME: u64 = 5;
+const NODE_2_WAIT_TIME: u64 = 3;
 
 /// Application State
 #[derive(Clone)]
@@ -66,35 +68,34 @@ impl EventHandler for FileServer {
 		println!("Connection established with peer: {:?}", peer_id);
 	}
 
-	// We need to handle the incoming rpc here. 
-	// What we're going to do is to look at out file system for the file specified in the rpc data and return it's binary content
+	// We need to handle the incoming rpc here.
+	// What we're going to do is to look at out file system for the file specified in the rpc data
+	// and return it's binary content
 	fn rpc_incoming_message_handled(&mut self, data: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
 		println!("Received incoming RPC: {:?}", data);
-	
+
 		// Extract the file name from the incoming data
 		let file_name = String::from_utf8_lossy(&data[0]);
 		let file_name = file_name.trim(); // Trim any potential whitespace
-	
+
 		// Read the file content
 		let mut file_content = Vec::new();
 		match File::open(&file_name) {
-			Ok(mut file) => {
-				match file.read_to_end(&mut file_content) {
-					Ok(_) => {
-						println!("File read successfully: {}", file_name);
-					}
-					Err(e) => {
-						println!("Failed to read file content: {}", e);
-						return vec![b"Error: Failed to read file content".to_vec()];
-					}
-				}
-			}
+			Ok(mut file) => match file.read_to_end(&mut file_content) {
+				Ok(_) => {
+					println!("File read successfully: {}", file_name);
+				},
+				Err(e) => {
+					println!("Failed to read file content: {}", e);
+					return vec![b"Error: Failed to read file content".to_vec()];
+				},
+			},
 			Err(e) => {
 				println!("Failed to open file: {}", e);
 				return vec![b"Error: Failed to open file".to_vec()];
-			}
+			},
 		}
-	
+
 		// Return the file content as a Vec<Vec<u8>>
 		vec![file_content]
 	}
@@ -110,13 +111,13 @@ impl EventHandler for FileServer {
 }
 
 /// Used to create a detereministic node 1.
-pub async fn setup_node_1(ports: (Port, Port)) -> Core<FileServer> {
+async fn setup_node_1(ports: (Port, Port)) -> Core<FileServer> {
 	let mut protobuf = PROTOBUF_KEYPAIR.clone();
 	setup_core_builder_1(&mut protobuf, ports).await
 }
 
 /// Setup node 2.
-pub async fn setup_node_2(
+async fn setup_node_2(
 	node_1_ports: (Port, Port),
 	ports: (Port, Port),
 ) -> (Core<FileServer>, PeerId) {
@@ -151,7 +152,7 @@ pub async fn setup_node_2(
 	)
 }
 
-pub async fn setup_core_builder_1(buffer: &mut [u8], ports: (u16, u16)) -> Core<FileServer> {
+async fn setup_core_builder_1(buffer: &mut [u8], ports: (u16, u16)) -> Core<FileServer> {
 	let app_state = FileServer;
 
 	// First, we want to configure our node by specifying a static keypair (for easy connection by
@@ -174,8 +175,7 @@ async fn run_node_1() {
 	let mut node = setup_node_1((49666, 49606)).await;
 
 	// Sleep for a few seconds to allow node 2 to reach out
-	// async_std::task::sleep(Duration::from_secs(NODE_1_WAIT_TIME)).await;
-	tokio::time::sleep(Duration::from_secs(NODE_1_WAIT_TIME)).await;
+	async_std::task::sleep(Duration::from_secs(NODE_1_WAIT_TIME)).await;
 
 	// What are we writing to the DHT?
 	// A file we have on the fs and the location of the file, so it can be easily retrieved
@@ -207,8 +207,7 @@ async fn run_node_2() {
 	let (mut node_2, node_1_peer_id) = setup_node_2((49666, 49606), (49667, 49607)).await;
 
 	// Sleep for a few seconds to allow node 1 write to the DHT
-	// async_std::task::sleep(Duration::from_secs(NODE_2_WAIT_TIME)).await;
-	tokio::time::sleep(Duration::from_secs(NODE_2_WAIT_TIME)).await;
+	async_std::task::sleep(Duration::from_secs(NODE_2_WAIT_TIME)).await;
 
 	// Prepare a query to read from the DHT
 	let kad_request = AppData::KademliaLookupRecord {
@@ -219,27 +218,24 @@ async fn run_node_2() {
 	if let Ok(result) = node_2.query_network(kad_request).await {
 		// We have our response
 		if let AppResponse::KademliaLookupSuccess(value) = result {
-			println!("We will be querying this file from the remote: {}", String::from_utf8_lossy(&value));
+			println!("File read from DHT: {}", String::from_utf8_lossy(&value));
 			// Now prepare an RPC query to fetch the file from the remote node
-			let fetch_key = vec![vec![0]];
-
-			println!("<<<< io! >>>");
+			let fetch_key = vec![value];
 
 			// prepare fetch request
 			let fetch_request = AppData::FetchData {
 				keys: fetch_key.clone(),
-				peer: node_1_peer_id.clone(),	// The peer to query for data
+				peer: node_1_peer_id.clone(), // The peer to query for data
 			};
-
-			println!("{:?}", fetch_request);
 
 			// We break the flow into send and recv explicitly here
 			let stream_id = node_2.send_to_network(fetch_request).await.unwrap();
 
 			// If we used `query_network(0)`, we won't have been able to print here
-			println!("A fetch request has been sent to peer: {:?}", node_1_peer_id);
-
-			node_2.recv_from_network(stream_id).await.unwrap();
+			println!(
+				"A fetch request has been sent to peer: {:?}",
+				node_1_peer_id
+			);
 
 			// Poll the network for the result
 			if let Ok(response) = node_2.recv_from_network(stream_id).await {
@@ -247,10 +243,12 @@ async fn run_node_2() {
 					// Get the file
 					let file = response_file[0].clone();
 
-					// Convert it to string 
+					// Convert it to string
 					let file_str = String::from_utf8_lossy(&file);
 
 					// Print to stdout
+					println!("Here is the file delivered from the remote peer:");
+					println!();
 					println!("{}", file_str);
 				}
 			} else {
@@ -260,8 +258,7 @@ async fn run_node_2() {
 	}
 }
 
-// #[async_std::main]
-#[tokio::main]
+#[async_std::main]
 async fn main() {
 	#[cfg(feature = "first-node")]
 	run_node_1().await;
